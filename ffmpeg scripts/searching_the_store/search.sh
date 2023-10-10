a720ba5bc431d54eab9be94c83ac9e0a818ef03e
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2023-10-10 16:26:15 +0200

    added search by absolute difference

---
+++ ffmpeg scripts/searching_the_store/search.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <audio_file> <measurement_to_sort>"
+    exit 1
+fi
+
+# Extract the arguments
+text_file=".output_data.txt"
+audio_file="$1.json"
+measurement_to_sort="$2"
+
+# Extract and sort the absolute differences for the specified audio file and measurement
+grep -w "$audio_file" "$text_file" | grep "$measurement_to_sort" | awk -F 'Absolute Difference: ' '{print "Difference: " $2, $1}' | sort -n
