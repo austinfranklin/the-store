97051b84a13efa801ca9a9a8206ebf9bb5843196
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2023-10-10 19:48:37 +0200

    fixed sorting

--- ffmpeg scripts/searching_the_store/search.sh
+++ ffmpeg scripts/searching_the_store/search.sh
@@ -12,4 +12,4 @@ audio_file="$1.json"
 measurement_to_sort="$2"
 
 # Extract and sort the absolute differences for the specified audio file and measurement
-grep -w "$audio_file" "$text_file" | grep "$measurement_to_sort" | awk -F 'Absolute Difference: ' '{print "Difference: " $2, $1}' | sort -n
+grep -w "$audio_file" "$text_file" | grep "$measurement_to_sort" | awk -F 'Absolute Difference: ' '{print $2, $1}' | sort -n
