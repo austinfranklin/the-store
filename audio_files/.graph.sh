bffd8a2072224d3aec644f6574525531113ade59
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2023-10-28 00:50:10 +0200

    Graphs for files and features and updated scripts

---
+++ audio_files/.graph.sh
@@ -0,0 +1,21 @@
+# formatting
+set origin -3, 0
+set tmargin 5
+set bmargin 15
+set size 4, 1
+set xtics rotate out
+set yrange[0:1]
+
+# labels
+set xlabel "Features/Files"
+set ylabel "Normalized Values"
+set title "Feature Extraction"
+
+# styling
+set boxwidth 0.75
+set style fill solid 1.0
+
+# use line number for x coordinate, column 1 for tic label
+#set term png
+#set output "$1.png"
+plot ".graph_file.txt" u 2:xtic(1) w boxes
--- ffmpeg scripts/searching_the_store/graph_plotting/plot.sh
+++ ffmpeg scripts/searching_the_store/graph_plotting/plot.sh
@@ -17,56 +17,51 @@ echo " " > "$graph_file"
 if [ "$type" == "file" ]; then
     # Extract and sort the values for the specified measurement
     grep "$argument" "$sort_file" | awk -F':' '{
-        if ($3 > 50)
-        print $2, ($3/20000);
-    else if ($3 > 1 && $3 < 50)
-        print $2, ($3/50);
-    else if ($3 < -0.001 && $3 > -0.1) 
-        print $2, ($3*-10);
-    else if ($3 < -0.1 && $3 > -1)
-        print $2, ($3*-1)
-    else if ($3 < -1)
-        print $2, ($3*-0.01)
-    else if ($3 > -0.001 && $3 < 0.001)
-        if ($3 < 0)
-            print $2, ($3*-1000);
-        else 
-            print $2, ($3*1000);
-        else
-        print $2, $3
-    }' | sort -n >> "$graph_file"
+        if ($3 > 1. && $3 < 50.)
+            print $2, ($3/50.);
+        else if ($3 > 50. && $3 < 10000.)
+            print $2, ($3/10000.);
+        else if ($3 > 10000. && $3 < 20000.)
+            print $2, ($3/20000.);
+        else if ($3 > 0 && $3 < 0.0000001)
+            print $2, ($3*5000000.);
+        else if ($3 < 0. && $3 > -0.0001)
+            print $2, ($3*-5000.);
+        else if ($3 < -0.01 && $3 > -1.)
+            print $2, $3*-1.5;
+        else if ($3 < -1.)
+            print $2, ($3/-96.);
+        else print $2, $3
+        }' | sort -n >> "$graph_file"
 
     # graph
-    gnuplot -p graph.sh
+    gnuplot -e "arg='$argument.png'" -p .graph.sh
 
 elif [ "$type" == "feature" ]; then
     # Extract and sort the values for the specified measurement
     grep "$argument" "$sort_file" | awk -F':' '{
-    if ($3 > 50)
-        print $1, ($3/20000);
-    else if ($3 > 1 && $3 < 50)
-        print $1, ($3/50);
-    else if ($3 < -0.001 && $3 > -0.1) 
-        print $1, ($3*-10);
-    else if ($3 < -0.1 && $3 > -1)
-        print $1, ($3*-1)
-    else if ($3 < -1)
-        print $1, ($3*-0.01)
-    else if ($3 > -0.001 && $3 < 0.001)
-        if ($3 < 0)
-            print $1, ($3*-1000);
-        else 
-            print $1, ($3*1000);
-        else
-        print $1, $3
-    }' | sort -n >> "$graph_file"
+        if ($3 > 1. && $3 < 50.)
+            print $1, ($3/50);
+        else if ($3 > 50. && $3 < 10000.)
+            print $1, ($3/10000.);
+        else if ($3 > 10000. && $3 < 20000.)
+            print $1, ($3/20000.);
+        else if ($3 > 0. && $3 < 0.0000001)
+            print $1, ($3*5000000.);
+        else if ($3 < 0. && $3 > -0.0001)
+            print $1, ($3*-5000.);
+        else if ($3 < -0.0001 && $3 > -1.)
+            print $1, $3*-1;
+        else if ($3 < -1.)
+            print $1, ($3/-96.);
+        else print $1, $3
+        }' | sort -n >> "$graph_file"
 
     # graph
-    gnuplot -p graph.sh
+    gnuplot -e "arg='$argument.png'" -p .graph.sh
         
 else
     # Handle the case where the wrong number of arguments is provided
     echo "Usage: $0 <type: 'file' or 'feature'> <file or feature to use>"
     exit 1
-
 fi
